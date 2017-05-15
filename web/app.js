867228a420bde64bf6e4388fab402ef07e7a6f0b
root <root@neos.com>
2017-05-15 15:38:27 +0200

    newweb

---
+++ web/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Bienvenue sur Neos LOL!');
+});
+
+app.listen(3000, function () {
+    console.log('En ecoute sur le port : 3000!');
+});
